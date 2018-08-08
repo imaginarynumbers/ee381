0e1317161d129c6b43f702726debde6b8695b822
Kacy Rowe <kace86@gmail.com>
2018-08-07 18:34:54 -0700

    Lab 7 - Part two

---
+++ Lab 7B.py
@@ -0,0 +1,12 @@
+# also look up how to use turtle for graphics in python, like fib sequence
+
+import matplotlib.pyplot as plt
+
+#x = [2, 8, 5]
+x = range(3)
+#plt.bar(x,100)
+
+plt.bar(x,[20, 50, 100])
+
+
+plt.show()
