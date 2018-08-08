ef081ee7236b0bacfafc67f7bca4829ec6f02025
Kacy Rowe <kace86@gmail.com>
2018-08-07 18:15:28 -0700

    Lab 1

---
+++ Lab1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 10:57:52 2018
+
+@author: kacyrowe
+"""
+
+a = "{} can be {}" .format("Strings","Interpolated")
+print(a)
